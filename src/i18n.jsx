38d507861913911f027380b4a5f1198575bf8583
RahmaanM17 <ardrah17@gmail.com>
2024-10-16 00:48:29 +0700

    feat: update project on my portfolio

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -29,7 +29,7 @@ i18n
           short_intro:
             "As a software engineer with 2.5 years experience, I focus on creating user-friendly solutions for complex challenges. I'm passionate about coding and staying updated on new tech trends. My goal is to deliver top-notch software that exceeds expectations, whether I'm working solo or with a team. I thrive in innovative environments, always looking to enhance my skills and push technology forward.",
           short_contact:
-            "Feel free to contact me for a chat or just some casual chit-chat! You can reach me at",
+            "Feel free to contact me for a chat or just some casual chit-chat and i'm open to work! You can reach me at",
         },
       },
       id: {
@@ -49,7 +49,7 @@ i18n
           short_intro:
             "Sebagai pengembang aplikasi dengan pengalaman 2,5 tahun, Saya fokus untuk menciptakan solusi yang ramah pengguna untuk tantangan yang kompleks, Saya antusias tentang pengkodean dan mempelajari teknologi baru. Tujuan saya adalah menghadirkan perangkat lunak terbaik yang melebihi ekspektasi. saya dapat bekerja sendiri atau dengan tim. Saya berkembang dalam lingkungan yang inovatif, selalu berupaya meningkatkan keterampilan saya dan mendorong kemajuan teknologi.",
           short_contact:
-            "Jangan ragu untuk menghubungi saya untuk ngobrol atau sekedar ngobrol santai! Anda dapat menghubungi saya di",
+            "Jangan ragu untuk menghubungi saya untuk ngobrol atau sekedar ngobrol santai dan saya siap berkerja! Anda dapat menghubungi saya di",
         },
       },
     },
